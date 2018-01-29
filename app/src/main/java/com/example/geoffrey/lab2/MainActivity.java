8b696c4a3395affe7aa0d0f37027803cf17f5f97
GeoffreyFT <35584311+GeoffreyFT@users.noreply.github.com>
2018-01-28 19:01:49 -0600

    Initial commit

---
+++ app/src/main/java/com/example/geoffrey/lab2/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.geoffrey.lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCI = new Intent(MainActivity.this, Child1.class);
+                startActivity(intentCI);
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCI = new Intent(MainActivity.this, Child2.class);
+                startActivity(intentCI);
+            }
+        });
+
+
+    }
+}
